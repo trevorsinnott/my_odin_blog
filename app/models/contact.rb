4248b605ea56391d214e7d01a917c6f1d9a6f7a3
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-08-26 16:54:53 +0900

    set up contact form

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < MailForm::Base
+  attributes :name,       :validate => true
+  attributes :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attributes :message,    :validate => true
+  attributes :nickname,   :captcha => true
+
+  def headers
+    {
+      :subject => "Contact Form",
+      :to => "sinnott.trevor@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
